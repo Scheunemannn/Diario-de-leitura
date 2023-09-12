880c1d36e5687a7cc0c02860ffb01f5f98017409
Eduardo Scheunemann <dudu.jefe@hotmail.com>
2023-09-12 13:27:07 -0300

    criando a landing page

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import styles from '../styles/header.module.css'
+import Link from 'next/link'
+
+export function Header () {
+    return (
+        <>
+            <header>
+                <ul className={styles.navbar}>
+                    <Link className={styles.navitem} href='./'>Home</Link>
+                    <Link className={styles.navitem} href='./about'>Obras</Link>
+                    <Link className={styles.navitem} href=''>About</Link>
+                </ul>
+            </header>
+        </>
+    )
+}
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import styles from "../styles/landingPage.module.css";
+
+export default function HomePage() {
+  return (
+    <>
+      <div className={styles.landingPage}>
+        <div className={styles.landingPageText}>
+        <p>⁠Não reze pelo futuro… Não pense nem no futuro e nem no passado, só se mantenha no presente.</p>
+        <p className={styles.vagabondReference}>-Vagabond</p>
+        </div>
+        <div className={styles.landingPageImage}>
+          <Image
+          src="/images/vagabond.png"
+          width={500}
+          height={700}
+          alt="vagabond"/>
+        </div>
+      </div>
+    </>
+  );
+}
